9a5c47b133a09a6cd32ba230360dc62a25ddcf22
dhruv1109 <dhruv11.bansal@gmail.com>
2022-04-25 11:50:49 +0530

    ClimbStairs_Recursion
    ClimbStairs_Recursion

---
+++ 08. Recursion/ClimbStairs_Recursion.cpp
@@ -0,0 +1,19 @@
+//recursive solution gives TLE
+#include<iostream>
+using namespace std;
+
+int climbStairs(int n) 
+{
+    if(n<0)
+        return 0;
+    if(n==0)
+        return 1;
+    return climbStairs(n-1)+climbStairs(n-2);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<climbStairs(n);
+}
