5c593424eeb980d712c424b9e97006521ffe4d11
dhruv1109 <dhruv11.bansal@gmail.com>
2022-10-24 21:51:27 +0530

    Leetcode Reverse List II
    Leetcode Reverse List II

---
+++ 07. Linked List/Revere_List_II.cpp
@@ -0,0 +1,40 @@
+// Leetcode Reverse LL II
+// https://leetcode.com/problems/reverse-linked-list-ii/
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+class Solution {
+public:
+    
+    ListNode* reverseBetween(ListNode* &head, int left, int right) {
+        
+        //If right == left then no need to reverse
+		if(!head or left == right)
+            return head;
+		
+		//inserting dummy node in front to handle the case when left =1
+        ListNode *dummy = new ListNode(0);
+        dummy->next =head;		
+		
+        ListNode *L ,*LEFT_PTR=dummy;
+        cout<<LEFT_PTR->val;
+        for(int i=0;i<left-1;i++)
+            LEFT_PTR = LEFT_PTR->next;
+		
+        L = LEFT_PTR->next;
+        
+        for(int i=0;i<right-left;i++){
+            ListNode *tmp  = LEFT_PTR->next;
+            LEFT_PTR->next = L->next;
+            L->next  =  L->next->next;
+            LEFT_PTR->next->next = tmp;
+        }
+        return dummy->next;
+    }
+};
