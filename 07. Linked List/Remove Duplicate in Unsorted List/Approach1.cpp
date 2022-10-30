afd17d23011cfb0eee90480bf1205077c2bc6d9f
dhruv1109 <dhruv11.bansal@gmail.com>
2022-10-30 23:14:26 +0530

    Remove Duplicate in Unsorted List
    Remove Duplicate in Unsorted List

---
+++ 07. Linked List/Remove Duplicate in Unsorted List/Approach1.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+Node *removeDuplicates(Node *head)
+{
+    // Write your code here
+    if(head==NULL)
+        return NULL;
+    
+    Node* next_next = NULL;    
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        Node* curr = temp;
+        while(curr->next != NULL)
+        {
+            if(temp->data == curr->next->data)
+            {
+                next_next = curr->next->next;
+                delete curr->next;
+                curr->next = curr->next->next;;
+            }
+            else
+            {
+                curr = curr->next;
+            }
+        }
+        temp = temp->next;
+    }
+    return head;    
+}
