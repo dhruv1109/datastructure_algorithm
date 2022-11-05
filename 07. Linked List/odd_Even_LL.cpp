594f5c416de106505f076108b1f70d91f8edd30c
dhruv1109 <dhruv11.bansal@gmail.com>
2022-11-05 12:04:45 +0530

    Odd Even LL
    Odd Even LL

---
+++ 07. Linked List/odd_Even_LL.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        
+        if(head==NULL || head->next==NULL || head->next->next==NULL)
+            return head;
+        
+        ListNode* oddhead=new ListNode();
+        ListNode* oddtail=oddhead;
+        ListNode* evenhead=new ListNode();
+        ListNode* eventail=evenhead;
+        
+        ListNode* temp=head;
+        int count=1;
+        while(temp)
+        {
+            if(count%2==1)
+            {
+                oddtail->next=temp;
+                oddtail=temp;
+            }
+            else
+            {
+                eventail->next=temp;
+                eventail=temp;
+            }
+            temp=temp->next;
+            count++;
+        }
+        oddtail->next=evenhead->next;
+        eventail->next=NULL;
+        
+        head=oddhead->next;
+        
+        delete oddhead;
+        delete evenhead;
+        
+        return head;       
+        
+    }
+};
